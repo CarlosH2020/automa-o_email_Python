42880539647070b974871d34b53dc08db183e6b1
Carlos Henrique Barros Silva Campos <63325025+CarlosH2020@users.noreply.github.com>
2021-04-11 22:12:03 -0300

    Update automacao_de_email.py

--- automacao_de_email.py
+++ automacao_de_email.py
@@ -4,13 +4,7 @@ Projeto de Automção de envio de E-Mail
 By Carlos Henrique Barros Silva Campos
 """
 import win32com.client as win32
-
-print('=' * 40)
-print('*                                      *')
-print('*         Automação de E-mail          *')
-print('*                                      *')
-print('=' * 40)
-
+import time
 
 # integração com outlook
 outlook = win32.Dispatch('outlook.application')
@@ -19,16 +13,50 @@ outlook = win32.Dispatch('outlook.application')
 email = outlook.CreateItem(0)
 
 
-# Configurar e-mail
+def menu():
+    print('=' * 40)
+    print('*         Automação de E-mail          *')
+    print('*                Menu:                 *')
+    print('=' * 40)
+    print('    (1 - Mandar apenas um E-mail.')
+    print('    (2 - Usar uma lista de E-mail. ')
+    print('    (3 - Sair.')
+    opc = int(input('Informe a opção:'))
+    if opc == 1:
+        pessoa = input('Para:')
+        titulo = input('Título:')
+        msg = input('Msg:')
+        configuraEmail(pessoa, titulo, msg)
+    elif opc == 2:
+        lista_de_email()
+    elif opc == 3:
+        exit(0)
+    else:
+        print('Opção incorreta!')
+        menu()
 
-email.to = "carlos.barros@aedb.br"
-email.Subject = "Teste de e-mail automção"
-email.HTMLBody = """
-<p>Testando envio de E-mail em Python</p>
 
-<p>Olá Carlos Henrique tudo bem... </p>
+def lista_de_email():
+    lista = input('Lista de E-mail:')
+    with open(lista) as lst:
+        lista = lst.read()
+        email.to = lista
+        titulo = input('Título:')
+        msg = input('Msg:')
+        email.Subject = titulo
+        email.HTMLBody = f"""<p>{msg}</p>"""
 
-"""
 
-print('E-mail enviado com sucesso!')
-email.Send()
+def configuraEmail(Email, titulo_msg, corpo_msg):
+    email.to = f"{Email};"
+    email.Subject = titulo_msg
+    email.HTMLBody = f"""<p>{corpo_msg}</p>"""
+
+    # anexo = "C://Users/Root/Desktop/Orçamento.xlsx"
+    # email.Attachments.Add(anexo)
+
+
+if __name__ == '__main__':
+    menu()
+    print('E-mail enviado com sucesso!')
+    email.Send()
